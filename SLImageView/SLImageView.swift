925dffa0e130967e6eb0fd19124428fd9739462f
DeviLJohn <KnustJohannes@googlemail.com>
2016-11-15 15:05:37 +0100

    Swift 3 conform

--- SLImageView/SLImageView.swift
+++ SLImageView/SLImageView.swift
@@ -25,7 +25,7 @@ class SLImageView: UIImageView {
     }
     
     private func setup() {
-        self.userInteractionEnabled = true
+        self.isUserInteractionEnabled = true
         let touchGesture = UITapGestureRecognizer(target: self, action: #selector(showFullscreen))
         self.addGestureRecognizer(touchGesture)
     }
@@ -34,11 +34,11 @@ class SLImageView: UIImageView {
 
         let tmpImageView = UIImageView(frame: self.frame)
         tmpImageView.image = self.image
-        tmpImageView.contentMode = UIViewContentMode.ScaleAspectFit
-        tmpImageView.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.8)
+        tmpImageView.contentMode = UIViewContentMode.scaleAspectFit
+        tmpImageView.backgroundColor = UIColor.black.withAlphaComponent(0.8)
         tmpImageView.tag = imageTag
         tmpImageView.alpha = 0.0
-        tmpImageView.userInteractionEnabled = true
+        tmpImageView.isUserInteractionEnabled = true
         
         let tap = UITapGestureRecognizer(target: self, action: #selector(hideFullscreen))
         tmpImageView.addGestureRecognizer(tap)
@@ -48,13 +48,13 @@ class SLImageView: UIImageView {
     
     private func createLabel() -> UILabel {
         
-        let label = UILabel(frame: CGRectZero)
+        let label = UILabel(frame: CGRect.zero)
         label.text = "Touch to hide"
         label.font = UIFont(name: "HelveticaNeue", size: 12.0)
         label.sizeToFit()
-        label.textAlignment = NSTextAlignment.Center
+        label.textAlignment = NSTextAlignment.center
         label.textColor = UIColor(white: 0.85, alpha: 1)
-        label.backgroundColor = UIColor.blackColor().colorWithAlphaComponent(0.8)
+        label.backgroundColor = UIColor.black.withAlphaComponent(0.8)
         label.alpha = 0.0
         
         return label
@@ -62,7 +62,7 @@ class SLImageView: UIImageView {
     
     func showFullscreen() {
         
-        let window = UIApplication.sharedApplication().windows.first!
+        let window = UIApplication.shared.windows.first!
         if window.viewWithTag(imageTag) == nil {
             
             self.fullscreenImageView = createFullscreenPhoto()
@@ -70,11 +70,11 @@ class SLImageView: UIImageView {
            
             let labelWidth = window.frame.size.width
             let labelHeight = closeLabel.frame.size.height + 16
-            self.closeLabel.frame = CGRectMake(0, window.frame.size.height - labelHeight, labelWidth, labelHeight)
+            self.closeLabel.frame =  CGRect.init(x: 0, y: window.frame.size.height - labelHeight, width: labelWidth, height: labelHeight)
             self.fullscreenImageView.addSubview(closeLabel)
             
             window.addSubview(self.fullscreenImageView)
-            UIView.animateWithDuration(0.4, delay: 0.0, options: .CurveEaseInOut, animations: {
+            UIView.animate(withDuration: 0.4, delay: 0.0, options: .curveEaseInOut, animations: {
                 
                 self.fullscreenImageView.frame = window.frame
                 self.fullscreenImageView.alpha = 1
@@ -88,7 +88,7 @@ class SLImageView: UIImageView {
     
     func hideFullscreen() {
         
-        UIView.animateWithDuration(0.4, delay: 0.0, options: .CurveEaseInOut, animations: {
+        UIView.animate(withDuration: 0.4, delay: 0.0, options: .curveEaseInOut, animations: {
             
             self.fullscreenImageView?.frame = self.frame
             self.fullscreenImageView?.alpha = 0
